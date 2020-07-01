4b6a878a422e39c5f34f8cd0b34d8c41cfd702a4
nakshayreddy <nanduakshayreddy@gmail.com>
2020-07-01 11:50:51 +0530

    ProGrad ID

--- src/app.js
+++ src/app.js
@@ -3,41 +3,43 @@
 //Debug the type of data provided
 //Return the types concatenated in a single variable
 function moreAboutHome(address, distanceFromTown, hasNeighbours) {
-
-    return address + distanceFromTown + hasNeighbours;
-
+  return typeof address + typeof distanceFromTown + typeof hasNeighbours;
 }
-moreAboutHome("amazon", 78, true);
-
+moreAboutHome("amazon", "seven", true);
 
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
 
 function moreAboutKaren(parents, noOfSiblings, isNuclearFamily) {
-    if (parent == String && noOfSibling == Number && isNuclearFamily == boolean) {
-        return true;
-    } else {
-        return false;
-
-    }
-
+  if (typeof parents == String) {
+    return true;
+  } else if (typeof noOfSiblings == Number) {
+    return true;
+  } else if (typeof isNuclearFamily == "boolean") {
+    return true;
+  } else {
+    return false;
+  }
 }
-moreAboutKaren("akash", 4, true)
 
-
-
+moreAboutKaren("akash", 4, true);
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
 function doesFriendExist(ageInText, ageInNumber) {
-    if (ageInNumber == "Nan") {
-        return ageInNumber;
-    }
+  if (ageInText !== NaN) {
+    return ageInText;
+  } else if (ageInNumber !== NaN) {
+    return ageInNumber;
+  } else if (ageInText === NaN) {
+    return NaN;
+  } else if (ageInText === " ") {
+    return NaN;
+  }
 }
-doesFriendExist("Nan", 2)
 
 //Progression 4:
 //Lily gave Karen x sweets
@@ -45,22 +47,44 @@ doesFriendExist("Nan", 2)
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
-function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) {
-    lilly = totalNoOfSweets - sweetsConsumedByKaren;
-    final = lilly - sweetsConsumedInNMeters;
-    return final;
-
+function sweetTooth(
+  totalNoOfSweets,
+  sweetsConsumedByKaren,
+  sweetsConsumedInNMeters,
+  metersToTravel
+) {
+  if (
+    typeof totalNoOfSweets !== "number" ||
+    typeof sweetsConsumedByKaren !== "number" ||
+    typeof sweetsConsumedInNMeters !== "number" ||
+    typeof metersToTravel !== "number"
+  ) {
+    return "No sweets for Karen's friend";
+  } else {
+    var leftSweets =
+      (totalNoOfSweets -
+        (sweetsConsumedByKaren + sweetsConsumedInNMeters * metersToTravel)) /
+      2;
+    return leftSweets;
+  }
 }
-sweetTooth(10, 4, 2, 2)
 
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
 function convertToCelsius(fahrenheit) {
-    let cel = (fahrenheit - 32) * 5 / 9;
+  if (
+    typeof fahrenheit === "undefined" ||
+    typeof fahrenheit === "string" ||
+    typeof fahrenheit === "object"
+  )
+    return "Technical Error!";
+  else {
+    var celsius = (fahrenheit - 32) * (5 / 9);
+    return celsius;
+  }
 }
-convertToCelsius(45)
-
+convertToCelsius(45);
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -70,26 +94,29 @@ convertToCelsius(45)
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
 function aDifficultChoice(choice) {
-    switch (choice) {
-        case 1:
-            console.log("Take her daughter to a doctor");
-            break;
-        case 2:
-            console.log("Talk to her husband about it");
-            break;
-        case 3:
-            console.log("Counsel her daughter herself");
-            break;
-        case 4:
-            console.log("Take her daughter to a doctor");
-            break;
-        case 5:
-            console.log("Lock her daughter in her room");
-            break;
-        default:
-            break;
-    }
-
+  switch (choice) {
+    case -1:
+      return "Break down and give up all hope";
+      break;
+    case 1:
+      return "Take her daughter to a doctor";
+      break;
+    case 2:
+      return "Talk to her husband about it";
+      break;
+    case 3:
+      return "Counsel her daughter herself";
+      break;
+    case 4:
+      return "Lock her daughter in her room";
+      break;
+    case "I give up":
+      return "Refused to do anything for Karen";
+      break;
+    case undefined:
+      return "Wasn't able to decide";
+      break;
+  }
 }
 aDifficultChoice(1);
 
@@ -102,10 +129,11 @@ aDifficultChoice(1);
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
 
-
 function consoleKaren(strategies) {
-    var str = strategies.length;
-    return str;
+  var str = strategies.length;
+  return str;
 }
 
-consoleKaren("Lily realized that she'd hurt her daughter All she wants now is for her to stop crying She refuses to talk to her but Lily doesn't stop trying She tries out multiple things hoping for the best");
+consoleKaren(
+  "Lily realized that she'd hurt her daughter All she wants now is for her to stop crying She refuses to talk to her but Lily doesn't stop trying She tries out multiple things hoping for the best"
+);
